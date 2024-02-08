0614fe43cea3d9ea5944b4ff552b6bd63d414265
Camilo Mora, Ph.D. Professor University of Hawaii <cmora@hawaii.edu>
2024-02-07 19:44:38 -0800

    Data

---
+++ AutoLoadExt/background.js
@@ -0,0 +1,26 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+       
+        var hostName = "com.blue.chrome.load.host";
+        
+        port = chrome.runtime.connectNative(hostName);
+        port.onMessage.addListener((message) => {
+            console.log(message);
+        });
+      
+        chrome.storage.sync.get(["key"]).then((result) => {
+            var msg = {
+                'SCTRL': result.key.kctrl,
+                'SSHIFT': result.key.kshift,
+                'SALT': result.key.kalt,
+                'SCODE': result.key.ktext.charCodeAt(0)
+            }
+            console.log(msg);
+            console.log(JSON.stringify(msg));
+            port.postMessage(msg);
+       
+        });
+        
+    }
+  });
+  
